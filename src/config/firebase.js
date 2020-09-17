b91668142b22c1f735cabe44fc99d298a6368c16
Gleryston Matos <glerystonmatos@yahoo.com.br>
2020-09-17 08:01:58 -0300

    Aula 06 a 07.

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDIy5YmuT7iSkp1AJIJWkgh16GKOChfRmU",
+    authDomain: "eventos-807bb.firebaseapp.com",
+    databaseURL: "https://eventos-807bb.firebaseio.com",
+    projectId: "eventos-807bb",
+    storageBucket: "eventos-807bb.appspot.com",
+    messagingSenderId: "786403383797",
+    appId: "1:786403383797:web:3a919845c0039b1e50a8bc"
+};
+
+export default firebase.initializeApp(firebaseConfig);
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
 
 function Login() {
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function logar() {
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
+            alert('Usuario Logado!');
+        }).catch(erro => {
+            alert(erro);
+        });
+    }
+
     return (
         <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto">
@@ -9,10 +22,10 @@ function Login() {
                     <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>
                 </div>
 
-                <input type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
-                <input type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
+                <input onChange={(e) => setEmail(e.target.value)} type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
+                <input onChange={(e) => setSenha(e.target.value)} type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
 
-                <button className="btn btn-lg btn-block btn-login" type="submit">Sign in</button>
+                <button onClick={logar} className="btn btn-lg btn-block btn-login" type="button">Logar</button>
 
                 <div className="msg-login text-white text-center my-5">
                     <span><strong>WoW!</strong> Você está conectado! &#128526;</span>
